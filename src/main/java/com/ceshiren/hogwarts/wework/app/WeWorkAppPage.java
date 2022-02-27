3613e5f284db93964481659b8e22204ffa551bf4
seveniruby <seveniruby@gmail.com>
2022-02-27 16:54:48 +0800

    测试用例结构改进

--- src/main/java/com/ceshiren/hogwarts/wework/app/WeWorkAppPage.java
+++ src/main/java/com/ceshiren/hogwarts/wework/app/WeWorkAppPage.java
@@ -33,4 +33,8 @@ public class WeWorkAppPage {
 
         return new ContactAppPage(driver);
     }
+
+    public void close(){
+        driver.quit();
+    }
 }
--- src/test/java/com/ceshiren/hogwarts/wework/app/ContactAppPageTest.java
+++ src/test/java/com/ceshiren/hogwarts/wework/app/ContactAppPageTest.java
@@ -1,6 +1,6 @@
 package com.ceshiren.hogwarts.wework.app;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.net.MalformedURLException;
 
@@ -10,9 +10,41 @@ import static org.hamcrest.Matchers.equalTo;
 
 class ContactAppPageTest {
 
+    private static WeWorkAppPage wework;
+
+    @BeforeAll
+    static void beforeAll() throws MalformedURLException {
+        //todo: 环境还原
+        //还原方式
+        //保证数据的唯一性
+        //UI自动化
+        //接口自动化
+        //数据库还原
+
+        wework = new WeWorkAppPage();
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        //如果进程被非正常中止，afterAll可能得不到执行
+        wework.close();
+
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        //单个用例的前置操作
+    }
+
+    @AfterEach
+    void afterEach(){
+        //单个用例的后置操作
+    }
+
     @Test
     void addMember() throws MalformedURLException {
-        WeWorkAppPage wework = new WeWorkAppPage();
+
         String mobile = generateMobile();
         String name = "ckjava22_" + mobile;
 
@@ -26,7 +58,7 @@ class ContactAppPageTest {
                 .searchMember(mobile)
                 .getProfile();
         assertThat(profile.name, equalTo(user1.name));
-        assertThat(profile.mail, containsString(user1.name+"@"));
+        assertThat(profile.mail, containsString(user1.name + "@"));
     }
 
     String generateMobile() {
